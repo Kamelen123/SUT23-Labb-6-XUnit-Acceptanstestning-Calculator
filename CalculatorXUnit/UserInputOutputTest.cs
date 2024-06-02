f34d78256ddfd829cbcf02e51a3d356e06e71ae1
Torbjörn <torbjorn.rodstrom@gmail.com>
2024-06-03 01:28:08 +0200

    bug fix

--- CalculatorXUnit/UserInputOutputTest.cs
+++ CalculatorXUnit/UserInputOutputTest.cs
@@ -23,5 +23,21 @@ namespace CalculatorXUnit
 
             Assert.Equal(expected, result);
         }
+        [Fact]
+        public void User_ShouldBeAbleToPerformSubtractionAndViewLogs()
+        {
+            
+            var inputReader = new StringReader("2\n30\n15\n5\n7\n");
+            var outputWriter = new StringWriter();
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            
+            UserInputOutput.Menu();
+            
+            var output = outputWriter.ToString();
+            Assert.Contains("30 - 15 = 15", output);
+            Assert.Contains("Logs:", output);
+            Assert.Contains("30 - 15 = 15", output);
+        }
     }
 }
